22537cae339bc02054f8f528ab15435c208eae13
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-05-13 16:48:02 -0400

    sign-up fails when it should

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,7 +20,29 @@ describe "user pages" do
   end
 
   describe "sign-up" do
+    
+    before { visit signup_path }
 
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Tyler Nisonoff"
+        fill_in "Email",        with: "tylernisonoff@gmail.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count)
+      end
+    end
   end
 
 end
