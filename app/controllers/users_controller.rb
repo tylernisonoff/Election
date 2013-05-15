857accc2f76eac7aeb315900408e0c9ae3a5f11d
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-05-15 10:07:49 -0400

    unsuccessful edits work properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,17 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      #handle successful
+    else
+      render 'edit'
+    end
+  end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,10 +25,11 @@ describe "Authentication" do
         click_button "Sign in"
       end
 
-      it {should have_selector('title', text: user.name) }
-      it {should have_link('Profile', href: user_path(user)) }
-      it {should have_link('Sign out', href: signout_path) }
-      it {should_not have_selector('Sign in', href: signin_path) }
+      it { should have_selector('title', text: user.name) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_selector('Sign in', href: signin_path) }
     end
   end
 end
